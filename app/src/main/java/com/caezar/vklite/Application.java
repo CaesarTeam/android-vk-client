f33d0d4d2fbe32d71489204905d8c32904be98b1
seva <seva213415@inbox.ru>
2018-03-26 02:00:41 +0300

    autorize and get token

---
+++ app/src/main/java/com/caezar/vklite/Application.java
@@ -0,0 +1,28 @@
+package com.caezar.vklite;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKAccessTokenTracker;
+import com.vk.sdk.VKSdk;
+
+/**
+ * Created by seva on 26.03.18 in 1:50.
+ */
+
+public class Application extends android.app.Application {
+
+    VKAccessTokenTracker vkAccessTokenTracker = new VKAccessTokenTracker() {
+        @Override
+        public void onVKAccessTokenChanged(VKAccessToken oldToken, VKAccessToken newToken) {
+            if (newToken == null) {
+                // VKAccessToken is invalid
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        vkAccessTokenTracker.startTracking();
+        VKSdk.initialize(this);
+    }
+}
--- app/src/main/java/com/caezar/vklite/activities/MainActivity.java
+++ app/src/main/java/com/caezar/vklite/activities/MainActivity.java
@@ -1,13 +1,19 @@
 package com.caezar.vklite.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.caezar.vklite.R;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
 
 public class MainActivity extends AppCompatActivity {
-
+    final String[] scope = new String[] {VKScope.MESSAGES};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView textView = findViewById(R.id.text123);
         textView.setText("Hello Vera");
+
+        VKSdk.login(this, scope);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                System.out.println(res.accessToken);
+                    // Пользователь успешно авторизовался
+            }
+            @Override
+            public void onError(VKError error) {
+                    // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
