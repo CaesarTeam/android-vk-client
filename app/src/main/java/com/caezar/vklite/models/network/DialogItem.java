9c12b0bffc1812412b71870b71cca6612c7a016f
seva <seva213415@inbox.ru>
2018-04-30 15:06:00 +0300

    remove Parcelable

--- app/src/main/java/com/caezar/vklite/models/network/DialogItem.java
+++ app/src/main/java/com/caezar/vklite/models/network/DialogItem.java
@@ -1,14 +1,11 @@
 package com.caezar.vklite.models.network;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
  * Created by seva on 11.04.18 in 15:08.
  */
 
 @SuppressWarnings({"unused"})
-public class DialogItem implements Parcelable {
+public class DialogItem {
     private int  unread;
     private int  in_read;
     private int  out_read;
@@ -58,37 +55,5 @@ public class DialogItem implements Parcelable {
                 ", out_read=" + out_read +
                 ", message_text=" + message +
                 '}';
-    }
-
-    protected DialogItem(Parcel in) {
-        unread = in.readInt();
-        in_read = in.readInt();
-        out_read = in.readInt();
-        message = in.readParcelable(DialogMessage.class.getClassLoader());
-    }
-
-    public static final Creator<DialogItem> CREATOR = new Creator<DialogItem>() {
-        @Override
-        public DialogItem createFromParcel(Parcel in) {
-            return new DialogItem(in);
-        }
-
-        @Override
-        public DialogItem[] newArray(int size) {
-            return new DialogItem[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(unread);
-        dest.writeInt(in_read);
-        dest.writeInt(out_read);
-        dest.writeParcelable(message, flags);
     }
 }
