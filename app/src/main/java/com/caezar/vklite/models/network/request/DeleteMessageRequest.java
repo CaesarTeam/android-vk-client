926d4e7a4f6bfc4bbfeb985437287f4025e6ea59
seva <seva213415@inbox.ru>
2018-05-01 16:25:11 +0300

    delete message model

---
+++ app/src/main/java/com/caezar/vklite/models/network/request/DeleteMessageRequest.java
@@ -0,0 +1,60 @@
+package com.caezar.vklite.models.network.request;
+
+import java.util.Arrays;
+
+/**
+ * Created by seva on 01.05.18 in 16:20.
+ */
+
+public class DeleteMessageRequest {
+    private int[] message_ids;
+    private boolean spam;
+    private boolean delete_for_all = true;
+
+    public int[] getMessage_ids() {
+        return message_ids;
+    }
+
+    public void setMessage_ids(int[] message_ids) {
+        this.message_ids = message_ids;
+    }
+
+    public boolean isSpam() {
+        return spam;
+    }
+
+    public void setSpam(boolean spam) {
+        this.spam = spam;
+    }
+
+    public boolean isDelete_for_all() {
+        return delete_for_all;
+    }
+
+    public void setDelete_for_all(boolean delete_for_all) {
+        this.delete_for_all = delete_for_all;
+    }
+
+    public DeleteMessageRequest() {
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        for (int value: message_ids) {
+            builder.append(value).append(",");
+        }
+
+        if (builder.length() > 0) {
+            builder.setLength(builder.length() - 1);
+        }
+
+        String message_ids = builder.toString();
+
+        return  "&message_ids=" + message_ids +
+                "&spam=" + spam +
+                "&delete_for_all=" + delete_for_all;
+    }
+}
